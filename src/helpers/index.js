9b7b0258557f48a5887e998f93bd12cf2be90e53
Renan Lopes <renanlopescoder@gmail.com>
2018-02-14 16:39:31 -0200

    Bugfix: Remove unused helper

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,9 +1,7 @@
 import ErrorTracker from './ErrorTracker';
 import If from './If';
-import removeJsonEmptyFields from './JsonHelper';
 
 export {
   ErrorTracker,
   If,
-  removeJsonEmptyFields,
 };
